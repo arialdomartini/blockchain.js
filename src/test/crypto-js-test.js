e354f738ce26378920777eb0751f5fefa9e6a95d
Arialdo Martini <arialdomartini@gmail.com>
2019-02-02 18:13:43 +0100

    Using crypto-js library in tests

--- src/test/crypto-js-test.js
+++ src/test/crypto-js-test.js
@@ -1,10 +1,18 @@
 var expect = require('chai').expect;
+var Crypto = require('crypto-js');
 
 describe("Crypto-js tests", function() {
     it("should calculate SHA256", function() {
-        var SHA256 = require("crypto-js/sha256");
-        var hashed = SHA256("Message", "Key").toString();
+        var hashed = Crypto.SHA256("Message", "Key").toString();
 
         expect(hashed).to.equal('2f77668a9dfbf8d5848b9eeb4a7145ca94c6ed9236e4a773f6dcafa5132b2f91');
     });
+
+    it("should encrypt strings and decrypt them back", function() {
+        var crypted = Crypto.AES.encrypt("secret message", "some secret").toString();
+
+        var decrypted = Crypto.AES.decrypt(crypted, "some secret").toString(Crypto.enc.Utf8);
+
+        expect(decrypted).to.equal("secret message");
+    })
 });
